01e0b0c2176d89fdeb5ae30f479d569ad69531ba
donkagunila <donkagunila@gmail.com>
2020-02-16 15:09:44 +0300

    ]added the routes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,7 @@
 
 @section('content')
 
-<div class="container-fluid">
-    <div class="row">
+
         {{-- the left part --}}
         <div class="col-md-6 left-part">
             
@@ -105,90 +104,6 @@
             </div>
         </div>
 
-        {{-- the right part --}}
-        <div class="col-md-6 right-part d-none d-md-block">
-            <div id="particles-js"></div>
-
-            <div class="slider-holder">
-                <div id="onboard" class="carousel slide" data-ride="carousel">
-
-                    <ol class="carousel-indicators">
-                        <li data-target="#onboard" data-slide-to="0" class="active"></li>
-                        <li data-target="#onboard" data-slide-to="1"></li>
-                        <li data-target="#onboard" data-slide-to="2"></li>
-                    </ol>
-
-
-                  <div class="carousel-inner slider-content">
-                    <div class="carousel-item active">
-                        <div class="slider-area">
-                            <div class="slider-image mb-5">
-                                <img src="{{ asset('img/ease.png')}}" class="d-block " alt="...">
-                            </div>
-
-                            <div class="slider-title mb-3">
-                                <h4 class="text-white">
-                                    Enroll with Ease
-                                </h4>
-                            </div>
-
-                            <div class="slider-words mb-11">
-                                <p class="text-white">
-                                    Usajili enrolls ou easily, <br> Neither physical availability nor paperwork is required.
-                                </p>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                    <div class="carousel-item">
-                       <div class="slider-area">
-                            <div class="slider-image mb-5">
-                                <img src="{{ asset('img/fast.png')}}" class="d-block " alt="...">
-                            </div>
-
-                            <div class="slider-title mb-3">
-                                <h4 class="text-white">
-                                    Fast Enrollment
-                                </h4>
-                            </div>
-
-                            <div class="slider-words mb-11">
-                                <p class="text-white">
-                                    With a click of a button, <br> usajili enrolls you instantly.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="carousel-item">
-                        <div class="slider-area">
-                            <div class="slider-image mb-5">
-                                <img src="{{ asset('img/multiple.png')}}" class="d-block " alt="...">
-                            </div>
-
-                            <div class="slider-title mb-3">
-                                <h4 class="text-white">
-                                    Multiple Enrolments
-                                </h4>
-                            </div>
-
-                            <div class="slider-words mb-11">
-                                <p class="text-white">
-                                    Usajili enrolls you in schools, trainings, Marathons<br>  search it, usajili enrolls you.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-
-                  </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+      
 
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,91 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+
+
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ $title }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+{{-- the left part --}}
+        <div class="col-md-6 left-part">
+            
+
+            <div class="form-holder">
+                    
+                    <div class="form-title mb-11">
+                        <div></div>
+
+                        <div>
+                            <h3>
+                                Forget Password
+                            </h3>
+                            <p>
+                               Oops, Lets recover that for you
+                            </p>
                         </div>
-                    @endif
+                    </div>
 
-                    <form method="POST" action="{{ route($passwordEmailRoute) }}">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <div class="form-body mt-4">
+                        <form method="POST" action="{{ route($passwordEmailRoute) }}">
+                            @csrf
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <div>
+                                <p class="mb-5 small">
+                                    Enter your email address for recovery, we will send you an email shortly
+                                </p>
                             </div>
-                        </div>
+
+                            <div class="input-group mb-4">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text {{ $errors->has('email') ? ' is-invalid' : '' }}">
+                                        <i class="fa fa-envelope"></i>
+                                    </span>
+                                </div>
+
+                                
+                                    <input id="email" 
+                                           type="email" 
+                                           class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" 
+                                           name="email" 
+                                           value="{{ old('email') }}" 
+                                           placeholder="Email Address" 
+                                           required autofocus>
+
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                
+                            </div>
+
+                          
+
+
+                        
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-8 offset-md-4 clear mb-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
+                                    {{ __('Reset') }}
+                                </button>                               
                             </div>
                         </div>
-                    </form>
-                </div>
+
+
+                         <div class="form-group mb-0">
+                            <div class="small text-center">
+                                New to <b>Usajili?</b> <a href="{{ route('register')}}" class="text-primary">Create account</a>                    
+                            </div>
+                        </div>
+
+                      </form>
+                    </div>
+
             </div>
         </div>
-    </div>
-</div>
+
+
 @endsection
